afbcb729dc6be21b86dd46bee2ca6017b87a24c5
Steve Biedermann <steve.biedermann@mquadr.at>
2015-06-05 08:13:59 +0200

    modified crosscompile script to package test keys

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-goxc -max-processors=$(grep -c ^processor /proc/cpuinfo) -include=README*,script/*,static/*,autoexec.json
+goxc -max-processors=$(grep -c ^processor /proc/cpuinfo) -include=README*,script/*,static/*,autoexec.json,keys/gen_test_key.sh,keys/server.crt,keys/server.key
